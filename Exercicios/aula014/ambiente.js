efcf512eda8fb753eca96cf077a2c6583e0c8a90
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-01 12:26:17 -0300

    exercicios debug

---
+++ Exercicios/aula014/ambiente.js
@@ -0,0 +1,38 @@
+// METODO FOR
+
+console.log(`VAI COMEÇAR!`)
+for (var c = 1; c <= 6; c++){
+    console.log(`tudo bem na linha ${c}`)
+}
+console.log(`Fim!`)
+
+
+/* METODO DO WHILE
+
+var c = 1 
+do {
+    console.log('tudo bem?')
+    c++
+} while (c <= 6)
+*/
+
+
+/* METODO JUST WHILE
+var c = 1 
+while (c <= 6){
+    console.log('tudo bem?')
+    c++
+}
+*/
+
+
+/* METODO RUIM
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
