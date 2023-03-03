20eacab80cdac1af02eda3f5a7363ae6f3137a00
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-03 10:23:21 -0300

    aprendendo a degugar no VS

--- CursoemVideo_Exercicios/aula015/d003 copy/scripts/script.js
+++ CursoemVideo_Exercicios/aula015/d003 copy/scripts/script.js
@@ -1,22 +1,23 @@
 function iniciar(){
 
 var numero = document.querySelector('input#itxnum').value
-var area = document.querySelector('textarea#area')
-
+var area = document.querySelector('select#area')
 
     if (numero == 0){
         window.alert (`Todo número mutiplicado por ZERO é igual a ZERO!`)
     } else{
         var calc = 0
         var res_calc = ''
-        var cont2 = 0
-        area.innerHTML = ``
-                
+
+        area.innerHTML = ''        
+        
         for (var cont = 1; cont <= 10; cont++){
             calc = numero * cont
-            res_calc = `${numero}x${cont} = ${calc}     `
-            
-            area.innerHTML += `${res_calc}` 
+
+            var opt = document.createElement('option')
+            opt.text = `${numero}x${cont} = ${calc}`
+            opt.value = `tab${cont}`
+            area.appendChild(opt) 
         }
     }
    
